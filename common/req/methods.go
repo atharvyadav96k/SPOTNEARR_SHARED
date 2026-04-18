a58623bb01c6e1715fbfa56abf6f7baf60ea2688
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-18 19:07:26 +0530

    request parser support

---
+++ common/req/methods.go
@@ -0,0 +1,11 @@
+package res
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func ParseBody(r *http.Request, v interface{}) error {
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(v)
+}
